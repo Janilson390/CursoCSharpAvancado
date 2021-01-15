ec1d7b725edc53fe9f1b33a0a4b851332d35d6dd
Janilson Florencio <janilson390@gmail.com>
2021-01-15 16:10:12 -0300

    Aula 1 - Introdução a Threads

---
+++ CursoCSharpAvancado/1_Introducao_Threads/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _1_Introducao_Threads
+{
+    class Program
+    {
+        static void FazerAlgo()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Terminei a tarefa!");
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(FazerAlgo));
+
+            t.Start();
+
+            Thread.Sleep(4000);
+            Console.WriteLine("Thread principal parada por 4 segundos!");
+
+            Console.ReadKey();
+        }
+    }
+}
